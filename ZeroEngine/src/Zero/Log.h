510c18407a97f5f6b41f235a6af5f98de5149459
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-05-13 18:52:52 +0530

    Added log.

---
+++ ZeroEngine/src/Zero/Log.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Core.h"
+#include<memory>
+//#include "spdlog\spdlog.h"
+#include "spdlog\spdlog.h"
+namespace Zero
+{
+	class ZERO_API Log
+	{
+	public:
+		static void Init();
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+
+	};
+}
+
+//Core log macros
+#define ZERO_CORE_TRACE(...)      ::Zero::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define ZERO_CORE_INFO(...)       ::Zero::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define ZERO_CORE_WARN(...)       ::Zero::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define ZERO_CORE_ERROR(...)      ::Zero::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define ZERO_CORE_FATAL(...)      ::Zero::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+//Client log macros
+#define ZERO_CLIENT_TRACE(...)    ::Zero::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define ZERO_CLIENT_INFO(...)     ::Zero::Log::GetClientLogger()->info(__VA_ARGS__)
+#define ZERO_CLIENT_WARN(...)     ::Zero::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define ZERO_CLIENT_ERROR(...)    ::Zero::Log::GetClientLogger()->error(__VA_ARGS__)
+#define ZERO_CLIENT_FATAL(...)    ::Zero::Log::GetClientLogger()->fatal(__VA_ARGS__)
