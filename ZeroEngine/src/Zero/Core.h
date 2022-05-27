72f5cfc320d4a314798d4ca8db64a770e9f876d8
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-05-27 21:34:14 +0530

    Added asserts in Core.h

--- ZeroEngine/src/Zero/Core.h
+++ ZeroEngine/src/Zero/Core.h
@@ -11,5 +11,14 @@
 
 #endif
 
+
+#ifdef ZERO_ENABLE_ASSERTS
+	#define ZERO_ASSERT(x,...) { if(!(x)) { ZERO_CLIENT_ERROR("Assertion failed : {0}",__VA_ARGS__); __debugbreak();}}
+	#define ZERO_CORE_ASSERT(x,...) { if(!(x)) { ZERO_CORE_ERROR("Assertion failed : {0}",__VA_ARGS__); __debugbreak();}}
+#else
+	#define ZERO_ASSERT(x,...)
+	#define ZERO_CORE_ASSERT(x,...)
+#endif
+
 //Left shifted for easy comparing.
 #define BIT(x) (1<<x)
