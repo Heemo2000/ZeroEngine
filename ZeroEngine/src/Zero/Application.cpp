ef336d26cb27776964b41e6a8406396957de4ca9
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-06-08 15:44:36 +0530

    Fixed PushOverlay method in Application class

--- ZeroEngine/src/Zero/Application.cpp
+++ ZeroEngine/src/Zero/Application.cpp
@@ -1,11 +1,17 @@
 #include "zeropch.h"
 #include "Application.h"
+#include <glad/glad.h>
 //#include "Events/ApplicationEvent.h"
 namespace Zero
 {
 #define BIND_EVENT_FN(x) std::bind(&Application::x,this,std::placeholders::_1)
+	
+	Application* Application::s_Instance = nullptr;
 	Application::Application()
 	{
+		ZERO_CORE_ASSERT(!s_Instance, "Application already exists!");
+
+		s_Instance = this;
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
@@ -18,13 +24,18 @@ namespace Zero
 	{
 		while (m_Running)
 		{
-			m_Window->OnUpdate();
+			glClearColor(0, 1, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			
 			std::vector<Layer*>::iterator it = m_LayerStack.end();
 			while (it != m_LayerStack.begin())
 			{
 				--it;
 				(*it)->OnUpdate();
+				//ZERO_CORE_INFO("Calling {0}", (*it)->GetLayerName());
 			}
+						
+			m_Window->OnUpdate();
 		}
 	}
 
@@ -43,29 +54,36 @@ namespace Zero
 			if (event.Handled)
 			{
 				break;
-			}
-			
-		}
+			}	
+		}		
+		
 	}
 
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
+
+		ZERO_CORE_INFO("Added {0}", layer->GetLayerName());
 	}
 
 	void Application::PopLayer(Layer* layer)
 	{
 		m_LayerStack.PopLayer(layer);
+		layer->OnDetach();
 	}
 
 	void Application::PushOverlay(Layer* overlay)
 	{
-		m_LayerStack.PopOverlay(overlay);
+		m_LayerStack.PushOverlay(overlay);
+		overlay->OnAttach();
+		ZERO_CORE_INFO("Added {0}", overlay->GetLayerName());
 	}
 
 	void Application::PopOverlay(Layer* overlay)
 	{
 		m_LayerStack.PopOverlay(overlay);
+		overlay->OnDetach();
 	}
 
 	bool Application::OnWindowClose(WindowClosedEvent& event)
--- ZeroEngine/src/Zero/Application.h
+++ ZeroEngine/src/Zero/Application.h
@@ -5,10 +5,21 @@
 #include "Window.h"
 #include "LayerStack.h"
 #include "Layer.h"
+
 namespace Zero
 {
 	class ZERO_API Application
 	{
+	private:
+		static Application* s_Instance;
+
+	private:
+		
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
+		bool OnWindowClose(WindowClosedEvent& event);
+		LayerStack m_LayerStack;
+
 	public:
 		Application();
 		virtual ~Application();
@@ -18,12 +29,11 @@ namespace Zero
 		void PopLayer(Layer* layer);
 		void PushOverlay(Layer* overlay);
 		void PopOverlay(Layer* overlay);
+	public:
+		inline static Application& GetInstance() { return *s_Instance; }
+		inline Window& GetWindow() { return *m_Window; }
 
-	private:
-		std::unique_ptr<Window> m_Window;
-		bool m_Running = true;
-		bool OnWindowClose(WindowClosedEvent& event);
-		LayerStack m_LayerStack;
+	
 	};
 
 	//To be defined in client.
