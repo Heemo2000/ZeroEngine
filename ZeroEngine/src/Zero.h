4023529a487ce7efdf7ed5a1bb20570adedea4c3
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-06-17 19:27:08 +0530

    Added key and mouse button codes.

--- ZeroEngine/src/Zero.h
+++ ZeroEngine/src/Zero.h
@@ -6,6 +6,11 @@
 #include "Zero/Log.h"
 #include "Zero/ImGui/ImGuiLayer.h"
 
+#include "Zero/KeyCodes.h"
+#include "Zero/MouseButtonCodes.h"
+#include "Zero/Input.h"
+
+
 //----Entry_Point---------------------------
 #include "Zero/EntryPoint.h"
 //------------------------------------------
