39d269c2604b79ff4ac666cc089229f3c0b1b866
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-06-20 17:17:42 +0530

    Enabled ZERO_ENABLE_ASSERTS for Debug configuration

--- ZeroEngine/src/Zero/Core.h
+++ ZeroEngine/src/Zero/Core.h
@@ -11,6 +11,10 @@
 
 #endif
 
+#ifdef ZERO_DEBUG
+	#define ZERO_ENABLE_ASSERTS
+#endif
+
 
 #ifdef ZERO_ENABLE_ASSERTS
 	#define ZERO_ASSERT(x,...) { if(!(x)) { ZERO_CLIENT_ERROR("Assertion failed : {0}",__VA_ARGS__); __debugbreak();}}
