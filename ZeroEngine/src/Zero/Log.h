82fef0aedfbda46d7eb330759b3c88d56aca2de2
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-07-09 09:34:14 +0530

    Fixed ZERO_CORE_FATAL and ZERO_CLIENT_FATAL
    The method name is "critical" instead of fatal in Log.h .

--- ZeroEngine/src/Zero/Log.h
+++ ZeroEngine/src/Zero/Log.h
@@ -21,11 +21,11 @@ namespace Zero
 #define ZERO_CORE_INFO(...)       ::Zero::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define ZERO_CORE_WARN(...)       ::Zero::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define ZERO_CORE_ERROR(...)      ::Zero::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define ZERO_CORE_FATAL(...)      ::Zero::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define ZERO_CORE_FATAL(...)      ::Zero::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 //Client log macros
 #define ZERO_CLIENT_TRACE(...)    ::Zero::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define ZERO_CLIENT_INFO(...)     ::Zero::Log::GetClientLogger()->info(__VA_ARGS__)
 #define ZERO_CLIENT_WARN(...)     ::Zero::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define ZERO_CLIENT_ERROR(...)    ::Zero::Log::GetClientLogger()->error(__VA_ARGS__)
-#define ZERO_CLIENT_FATAL(...)    ::Zero::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define ZERO_CLIENT_FATAL(...)    ::Zero::Log::GetClientLogger()->critical(__VA_ARGS__)
